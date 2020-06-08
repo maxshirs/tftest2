8df7a2c653f28b94a955e3f316951aceae0e52b1
root <max.shirshov@protonmail.com>
2020-06-08 13:25:52 +0000

    Mon Jun 08 2020 - Committed from Codeherent platform

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
-
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
 }
-
-
 
 output "clb_dns_name" {
   value       = aws_elb.example.dns_name
@@ -13,32 +10,33 @@ output "clb_dns_name" {
 }
 
 provider "aws" {
- region = "us-east-2"
+  region = "us-east-2"
 }
-
-
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
-
   ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = var.server_port
+    to_port   = var.server_port
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
   }
 }
 
 resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
+  image_id      = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  security_groups = [
+    aws_security_group.instance.id,
+  ]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
-    lifecycle {
+  lifecycle {
     create_before_destroy = true
   }
 }
@@ -46,13 +44,12 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.id
   availability_zones   = data.aws_availability_zones.all.names
-
-  min_size = 2
-  max_size = 10
-
-  load_balancers    = [aws_elb.example.name]
+  min_size             = 2
+  max_size             = 10
+  load_balancers = [
+    aws_elb.example.name,
+  ]
   health_check_type = "ELB"
-
   tag {
     key                 = "Name"
     value               = "terraform-asg-example"
@@ -63,20 +60,18 @@ resource "aws_autoscaling_group" "example" {
 data "aws_availability_zones" "all" {}
 
 resource "aws_elb" "example" {
-  name               = "terraform-asg-example"
-  security_groups    = [aws_security_group.elb.id]
+  name = "terraform-asg-example"
+  security_groups = [
+    aws_security_group.elb.id,
+  ]
   availability_zones = data.aws_availability_zones.all.names
-
-
   health_check {
-   target              = "HTTP:${var.server_port}/"
-   interval            = 30
-   timeout             = 3
-   healthy_threshold   = 2
-   unhealthy_threshold = 2
- }
-
-
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
   # This adds a listener for incoming HTTP requests.
   listener {
     lb_port           = 80
@@ -87,17 +82,46 @@ resource "aws_elb" "example" {
 }
 
 resource "aws_security_group" "elb" {
-  name = "terraform-example-elb"  # Allow all outbound
+  name = "terraform-example-elb" # Allow all outbound
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  # Inbound HTTP from anywhere
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  } # Inbound HTTP from anywhere
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
   }
+}
+
+module "ec2-instance-max" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.13.0"
+  # A list of security group IDs to associate with
+  vpc_security_group_ids = []
+  # The type of instance to start
+  instance_type = ""
+  # Name to be used on all resources as prefix
+  name = ""
+  # Specify one or more IPv6 addresses from the range of the subnet to associate with the primary network interface
+  ipv6_addresses = []
+  # ID of AMI to use for the instance
+  ami = ""
+  # If true, the EC2 instance will have associated public IP address
+  associate_public_ip_address = true
+  # A number of IPv6 addresses to associate with the primary network interface. Amazon EC2 chooses the IPv6 addresses from the range of your subnet.
+  ipv6_address_count = 1
+  # Can be used instead of user_data to pass base64-encoded binary data directly. Use this instead of user_data whenever the value is not a valid UTF-8 string. For example, gzip-encoded user data must be base64-encoded and passed via this argument to avoid corruption.
+  user_data_base64 = ""
+  # The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead.
+  user_data = ""
+  # Private IP address to associate with the instance in a VPC
+  private_ip = ""
 }
